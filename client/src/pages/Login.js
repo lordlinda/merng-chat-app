e4ddb1af7bd33d38c0cf5b21d09d60e0046aa701
lord <marialindananono@gmail.com>
2020-12-09 15:30:46 +0300

    error handling

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Form } from "semantic-ui-react";
 import { gql, useMutation } from "@apollo/client";
 import { useForm } from "../utils/hooks";
 import { AuthContext } from "../utils/auth";
 function Login(props) {
+  const [error, setError] = useState("");
   const { login } = useContext(AuthContext);
   const { onChange, onSubmit, values } = useForm(LoginUser, {
     username: "",
@@ -11,12 +12,11 @@ function Login(props) {
   });
   const [loginUser, { loading }] = useMutation(LOGIN_USER, {
     update(proxy, result) {
-      console.log(result);
       login(result.data.login);
       props.history.push("/");
     },
     onError(err) {
-      console.log(err);
+      setError(err.networkError.result.errors[0].message);
     },
     variables: values,
   });
@@ -47,7 +47,7 @@ function Login(props) {
           value={password}
           onChange={onChange}
         />
-
+        {error && <p style={{ color: "red" }}>{error}</p>}
         <Button type="submit" primary>
           Register
         </Button>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Form } from "semantic-ui-react";
 import { gql, useMutation } from "@apollo/client";
 import { useForm } from "../utils/hooks";
 import { AuthContext } from "../utils/auth";
 
 function Register(props) {
+  const [error, setError] = useState("");
   const { login } = useContext(AuthContext);
   /**the reason we call registerUser instead of addUser is because it is not declared yet */
   const { onChange, onSubmit, values } = useForm(registerUser, {
@@ -22,8 +23,8 @@ function Register(props) {
       login(result.data.register);
       props.history.push("/");
     },
-    onError(err) {
-      console.log(err);
+    onError(error) {
+      setError(error.networkError.result.errors[0].message);
     },
     variables: values,
   });
@@ -69,6 +70,7 @@ function Register(props) {
           value={confirmPassword}
           onChange={onChange}
         />
+        {error && <p style={{ color: "red" }}>{error}</p>}
         <Button type="submit" primary>
           Register
         </Button>
